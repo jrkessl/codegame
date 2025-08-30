85d3e46b359ee5b1484e6fb7ac1dbf92786e0444
Juliano Kessler <juliano.kessler@ticketmaster.com>
2025-08-30 17:00:41 -0300

    minor improvements, and with the window scaling logic

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import pygame
+from pygame.constants import HWSURFACE, DOUBLEBUF, RESIZABLE
+from pygame.surface import Surface
 
 pygame.init()
 
-# Load image and create window
-image = pygame.image.load("grass2.png") # load image 
-window = pygame.display.set_mode(image.get_size()) # create window the size of the image
+# Load background image and create window
+window = pygame.display.set_mode((1024, 768), HWSURFACE | DOUBLEBUF | RESIZABLE) # Creates game window with set size, and some options 
 pygame.display.set_caption("Code Game (working title)") # set window title
+background = pygame.image.load("grass2.png") # load background image
+renderWidth = background.get_width() # capture dimensions of background image
+renderHeight = background.get_height()
 
 running = True
 while running:
@@ -20,9 +24,31 @@ while running:
                 running = False
                 break
 
-    # Render
-    window.blit(image, (0, 0))
-    pygame.display.update()
+    # Create a surfate and blit the background image onto it
+    renderSurface = Surface((renderWidth, renderHeight))
+    renderSurface.blit(background, (0, 0))
+
+    # Scale rendering to window size
+    windowWidth, windowHeight = window.get_size()
+    renderRatio = renderWidth / renderHeight
+    windowRatio = windowWidth / windowHeight
+    if windowRatio <= renderRatio:
+        rescaledSurfaceWidth = windowWidth
+        rescaledSurfaceHeight = int(windowWidth / renderRatio)
+        rescaledSurfaceX = 0
+        rescaledSurfaceY = (windowHeight - rescaledSurfaceHeight) // 2
+    else:
+        rescaledSurfaceWidth = int(windowHeight * renderRatio)
+        rescaledSurfaceHeight = windowHeight
+        rescaledSurfaceX = (windowWidth - rescaledSurfaceWidth) // 2
+        rescaledSurfaceY = 0
+
+    # Scale the rendering to the window/screen size
+    rescaledSurface = pygame.transform.scale(
+        renderSurface, (rescaledSurfaceWidth, rescaledSurfaceHeight)
+    )
+    window.blit(rescaledSurface, (rescaledSurfaceX, rescaledSurfaceY)) # render the scaled surface in the window
+    pygame.display.update() # update the display
 
 pygame.quit()
 
