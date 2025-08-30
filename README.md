ea6a0dd76d755a873d853b2413d6be9fea8e1c1d
Juliano Kessler <juliano.kessler@ticketmaster.com>
2025-08-30 15:16:07 -0300

    trying something...

---
+++ README.md
@@ -0,0 +1,4 @@
+python3 -m venv venv0 # just once
+source venv0/bin/activate
+
+pip install pygame-ce # just once
---
+++ main.py
@@ -0,0 +1,33 @@
+import pygame
+
+pygame.init()
+
+# Load image and create window
+image = pygame.image.load("gray.png")
+# image = pygame.image.load("/home/juliano/gh/codegame/gray.png")
+window = pygame.display.set_mode(image.get_size())
+
+running = True
+while running:
+
+    # Handle input
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            break
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
+                break
+
+    # Render
+    window.blit(image, (0, 0))
+    pygame.display.update()
+
+pygame.quit()
+
+# parou em:
+# https://www.patternsgameprog.com/strategy-game-1-start
+
+# isso aqui parece interessante:
+# https://github.com/Murasakiao/RTS_pygame
